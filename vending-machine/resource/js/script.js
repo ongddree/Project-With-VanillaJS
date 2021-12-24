862f9903009ad3e71a4bc204eb5ec16c8f6f0dc4
ongddree <ongddree@gmail.com>
2021-12-25 04:03:17 +0900

    :sparkles: feat: 콜라 아이템 버튼 활성화

--- vending-machine/resource/js/script.js
+++ vending-machine/resource/js/script.js
@@ -6,6 +6,7 @@ const btnDeposit = document.getElementById("btnDeposit");
 const btnRefund = document.getElementById("btnRefund");
 const balanceStr = document.getElementById("balance");
 const totalAmo = document.getElementById("totalAmount");
+const btnCola = Array.from(document.getElementsByClassName("cola__btn"));
 
 function AmountCommas(val) {
   return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -23,21 +24,44 @@ function addComma(num) {
   num = String(num);
   return num.replace(/(\d)(?=(?:\d{3})+(?!\d))/g, "$1,");
 }
+
+console.log(btnCola[0].disabled);
+//콜라 아이템 버튼 활성화
+
+function doEnabled() {
+  btnCola.forEach((item) => {
+    if (sumDeposit >= 1000) {
+      item.classList.add("enabled");
+      item.disabled = false;
+      console.log(btnCola[0].disabled);
+    } else {
+      item.classList.remove("enabled");
+      item.disabled = true;
+    }
+  });
+}
+
 //입금기능
 let sumDeposit = 0;
 
 function doDeposit() {
-  //방지: 소지금 초과, 문자입력 알림창
+  //방지: 소지금 초과, 문자입력 알림창, 마이너스 금액 입력
   if (+inpDeposit.value > cash) {
     inpDeposit.value = "";
     return alert("소지금을 초과하였습니다.");
   } else if (isNaN(inpDeposit.value)) {
     inpDeposit.value = "";
     return alert("숫자를 입력하세요.");
+  } else if (+inpDeposit.value < 0) {
+    inpDeposit.value = "";
+    return alert("마이너스로 시작하는 금액은 없습니다.");
   }
+
   //입금액 반영
   sumDeposit += +inpDeposit.value;
   cash -= +inpDeposit.value;
+
+  doEnabled();
 
   //출력: 입금액
   balanceStr.innerText = `${addComma(sumDeposit)}원`;
